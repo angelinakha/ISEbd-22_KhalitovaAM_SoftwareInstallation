1ab6155aa13972d939d6c27180ef6342c38fb163
Angelina <khalitova.ang@gmail.com>
2022-03-20 22:37:22 +0400

    lr4

---
+++ SoftwareInstallation/SoftwareInstallationBusinessLogic/OfficePackage/AbstractSaveToExcel.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SoftwareInstallationBusinessLogic.OfficePackage.HelperEnums;
+using SoftwareInstallationBusinessLogic.OfficePackage.HelperModels;
+
+namespace SoftwareInstallationBusinessLogic.OfficePackage
+{
+    public abstract class AbstractSaveToExcel
+    {
+        // Создание отчета
+        public void CreateReport(ExcelInfo info)
+        {
+            CreateExcel(info);
+            InsertCellInWorksheet(new ExcelCellParameters
+            {
+                ColumnName = "A",
+                RowIndex = 1,
+                Text = info.Title,
+                StyleInfo = ExcelStyleInfoType.Title
+            });
+            MergeCells(new ExcelMergeParameters
+            {
+                CellFromName = "A1",
+                CellToName = "C1"
+            });
+            uint rowIndex = 2;
+            foreach (var pc in info.PackageComponents)
+            {
+                InsertCellInWorksheet(new ExcelCellParameters
+                {
+                    ColumnName = "A",
+                    RowIndex = rowIndex,
+                    Text = pc.ComponentName,
+                    StyleInfo = ExcelStyleInfoType.Text
+                });
+                rowIndex++;
+                foreach (var package in pc.Packages)
+                {
+                    InsertCellInWorksheet(new ExcelCellParameters
+                    {
+                        ColumnName = "B",
+                        RowIndex = rowIndex,
+                        Text = package.Item1,
+                        StyleInfo = ExcelStyleInfoType.TextWithBroder
+                    });
+                    InsertCellInWorksheet(new ExcelCellParameters
+                    {
+                        ColumnName = "C",
+                        RowIndex = rowIndex,
+                        Text = package.Item2.ToString(),
+                        StyleInfo = ExcelStyleInfoType.TextWithBroder
+                    });
+                    rowIndex++;
+                }
+                InsertCellInWorksheet(new ExcelCellParameters
+                {
+                    ColumnName = "C",
+                    RowIndex = rowIndex,
+                    Text = pc.TotalCount.ToString(),
+                    StyleInfo = ExcelStyleInfoType.Text
+                });
+                rowIndex++;
+            }
+            SaveExcel(info);
+        }
+        // Создание excel-файла
+        protected abstract void CreateExcel(ExcelInfo info);
+        // Добавляем новую ячейку в лист
+        protected abstract void InsertCellInWorksheet(ExcelCellParameters excelParams);
+        // Объединение ячеек
+        protected abstract void MergeCells(ExcelMergeParameters excelParams);
+        // Сохранение файла
+        protected abstract void SaveExcel(ExcelInfo info);
+    }
+}
---
+++ SoftwareInstallation/SoftwareInstallationContracts/ViewModels/ReportPackageComponentViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftwareInstallationContracts.ViewModels
+{
+    public class ReportPackageComponentViewModel
+    {
+        public string ComponentName { get; set; }
+        public int TotalCount { get; set; }
+        public List<Tuple<string, int>> Packages { get; set; }
+
+    }
+}
