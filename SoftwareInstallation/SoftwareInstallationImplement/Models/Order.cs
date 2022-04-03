22599e9156d3f281db1eb494dee37cd680876352
Angelina <khalitova.ang@gmail.com>
2022-04-03 22:50:21 +0400

    l5

--- SoftwareInstallation/SoftwareInstallationImplement/Models/Order.cs
+++ SoftwareInstallation/SoftwareInstallationImplement/Models/Order.cs
@@ -15,6 +15,7 @@ namespace SoftwareInstallationListImplement.Models
         public int PackageId { get; set; }
         public int Count { get; set; }
         public decimal Sum { get; set; }
+        public int ClientId { get; set; }
         public OrderStatus Status { get; set; }
         public DateTime DateCreate { get; set; }
         public DateTime? DateImplement { get; set; }
---
+++ SoftwareInstallation/SoftwareInstallationRestApi/Controllers/MainController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SoftwareInstallationContracts.BindingModels;
+using SoftwareInstallationContracts.BusinessLogicsContracts;
+using SoftwareInstallationContracts.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SoftwareInstallationRestApi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class MainController : ControllerBase
+    {
+        private readonly IOrderLogic _order;
+        private readonly IPackageLogic _package;
+        public MainController(IOrderLogic order, IPackageLogic package)
+        {
+            _order = order;
+            _package = package;
+        }
+        [HttpGet]
+        public List<PackageViewModel> GetProductList() => _package.Read(null)?.ToList();
+        [HttpGet]
+        public PackageViewModel GetProduct(int packageId) => _package.Read(new PackageBindingModel
+        { Id = packageId })?[0];
+        [HttpGet]
+        public List<OrderViewModel> GetOrders(int clientId) => _order.Read(new OrderBindingModel
+        { ClientId = clientId });
+        [HttpPost]
+        public void CreateOrder(CreateOrderBindingModel model) =>
+       _order.CreateOrder(model);
+    }
+}
