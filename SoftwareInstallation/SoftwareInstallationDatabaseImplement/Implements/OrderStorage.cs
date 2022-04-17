6b4a561ded5bfd5c38e1c942d1b0ccd5737235b8
Angelina <khalitova.ang@gmail.com>
2022-04-17 19:36:35 +0400

    5 laba done

--- SoftwareInstallation/SoftwareInstallationDatabaseImplement/Implements/OrderStorage.cs
+++ SoftwareInstallation/SoftwareInstallationDatabaseImplement/Implements/OrderStorage.cs
@@ -31,7 +31,11 @@ namespace SoftwareInstallationDatabaseImplement.Implements
             using var context = new SoftwareInstallationDatabase();
             return context.Orders
             .Include(rec => rec.Package)
-            .Where(rec => rec.Id.Equals(model.Id) || rec.DateCreate >= model.DateFrom && rec.DateCreate <= model.DateTo)
+            .Where(rec => (!model.DateFrom.HasValue && !model.DateTo.HasValue &&
+rec.DateCreate.Date == model.DateCreate.Date) ||
+ (model.DateFrom.HasValue && model.DateTo.HasValue && rec.DateCreate.Date
+>= model.DateFrom.Value.Date && rec.DateCreate.Date <= model.DateTo.Value.Date) ||
+ (model.ClientId.HasValue && rec.ClientId == model.ClientId))
             .Select(CreateModel)
             .ToList();
         }
